1b797ac0fead4da50b66f8039f204f20a2e14cf4
farhanganteng11 <2310631170017@student.unsika.ac.id>
2025-06-24 13:35:23 +0700

    Delete tugaspbw/p11 directory

--- tugaspbw/p11/index.php
+++
@@ -1,8 +0,0 @@
-<!-- Session -->
-<?php
-    session_start();
-    if (!isset($_SESSION['login_Un51k4'])) {
-        header("Location: login.php?message=" . urlencode("Untuk mengakses fitur anda harus login."));
-    exit;
-    }
-?>
